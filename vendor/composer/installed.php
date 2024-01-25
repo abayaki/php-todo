b486c94c4606a4f350e5de7465d8864543d59d9f
Ubuntu <ubuntu@ip-172-31-34-127.us-east-2.compute.internal>
2024-01-25 07:20:34 +0000

    Update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '59ec4ce6d056f20be24e143c32804d667656e4ee',
+        'reference' => 'c5336c406e04ffe1763cb09cbd6a024333f2e88f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -325,7 +325,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '59ec4ce6d056f20be24e143c32804d667656e4ee',
+            'reference' => 'c5336c406e04ffe1763cb09cbd6a024333f2e88f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
